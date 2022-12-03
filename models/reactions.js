6aefafd0fec30974c73031b90040941d41f223d5
ramlaahmed <ramlaahmed01@gmail.com>
2022-12-03 19:31:16 +0000

    added mongoose models

---
+++ models/reactions.js
@@ -0,0 +1,37 @@
+const { Schema, Types } = require("mongoose");
+const moment = require("moment");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: formatDate,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    _id: false,
+  }
+);
+
+function formatDate(date) {
+  return moment(date).format("DD/MM/YY HH:mm");
+}
+
+module.exports = reactionSchema;
