05fe096f68246fb2289d9997d05bf6e84795ec97
ramlaahmed <ramlaahmed01@gmail.com>
2022-12-04 01:29:58 +0000

    changed the path

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./routes/api/index.js');
 
-router.use('routes/api/index.js', apiRoutes);
+router.use('./api', apiRoutes);
 
 router.use((req, res) => res.send('this is a wrong route!'));
 
